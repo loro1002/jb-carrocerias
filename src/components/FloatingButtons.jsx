145b11dff65f49c19dd10bc9be444ab447387749
loro1002 <157169594+loro1002@users.noreply.github.com>
2024-07-31 13:06:48 -0300

    Atualiza componentes AboutUs, ServiceCard e Gallery com melhorias de layout e funcionalidade

--- src/components/FloatingButtons.jsx
+++ src/components/FloatingButtons.jsx
@@ -1,14 +1,23 @@
-
-import { FaWhatsapp, FaInstagram } from 'react-icons/fa';
-import './FloatingButtons.css'; // Certifique-se de adicionar estilos apropriados
+import { FaWhatsapp, FaInstagram } from "react-icons/fa";
+import "./FloatingButtons.css"; // Certifique-se de adicionar estilos apropriados
 
 const FloatingButtons = () => {
   return (
     <div className="floating-buttons">
-      <a href="https://wa.me/5544984225987" target="_blank" rel="noopener noreferrer" className="floating-button whatsapp">
+      <a
+        href="https://wa.me/5544984225987?text=vim%20do%20site%20e%20gostaria%20de%20mais%20informações."
+        target="_blank"
+        rel="noopener noreferrer"
+        className="floating-button whatsapp"
+      >
         <FaWhatsapp />
       </a>
-      <a href="https://www.instagram.com/carroceriasjb/" target="_blank" rel="noopener noreferrer" className="floating-button instagram">
+      <a
+        href="https://www.instagram.com/carroceriasjb/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="floating-button instagram"
+      >
         <FaInstagram />
       </a>
     </div>
